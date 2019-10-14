f76f50110d2829c626d807d7e14aea75b3608911
ThomasBtl <47348222+ThomasBtl@users.noreply.github.com>
2019-10-14 22:54:20 +0200

    Filling algo implemented
    I've made some tests, it should work just fine.
    Cleaning can be done.
    More testing needed to find potential bugs
    (Ouai je sais j'écris en anglais je suis trop bilingue)

--- src/game_logic/game.py
+++ src/game_logic/game.py
@@ -32,16 +32,31 @@ class Game:
             return False
         if case_value == 0:
             self.update_case(xy, player)
-
+        x,y = xy
+        zone_right = set()
+        zone_left = set()
+        delta = (x - player.x, y - player.y)
+        if(x == 0 or x == 7 or self.get_case((x + delta[0], y + delta[1])) == player.id):
+            zone_right = self.get_zone((x, y - 1), player.id, set((x,y)))
+            zone_left = self.get_zone((x, y + 1), player.id, set((x,y)))
+        elif(y == 0 or y == 7 or self.get_case((x + delta[0], y + delta[1])) == player.id):
+            zone_right = self.get_zone((x + 1, y), player.id, set((x,y)))
+            zone_left = self.get_zone((x - 1, y), player.id, set((x,y)))
+        if zone_left != -1:
+            self.fill_zone(zone_left, player)
+        if zone_right != -1:
+            self.fill_zone(zone_right, player)
         return True
 
     def start_game(self):
         self.players = Player(1, 0, 0), Player(2, 7, 7)
-        if self.validate((0,0), self.players[0]) and self.validate((self.size_x - 1,self.size_y - 1), self.players[1]):
-            self.players[0].xy = (0,0)
-            self.players[1].xy = (self.size_x - 1, self.size_y - 1)
-        else:
-            return -1
+
+        #I removed starting location validation, we can assume that we are not going to put the player on any comprise case
+        self.update_case((0,0), self.players[0])
+        self.update_case((self.size_x - 1, self.size_y - 1), self.players[1])
+        self.players[0].xy = (0,0)
+        self.players[1].xy = (self.size_x - 1, self.size_y - 1)
+       
         self.game_state = 1
         while self.game_state == 1:
             #Player 1's turn
@@ -67,3 +82,40 @@ class Game:
                 elif case == 2:
                     print(" ■ ", end="")
             print("\n")
+
+    """
+    Method that check if a zone is accessible by an other player
+    Args :
+        - xy : coord xy of the player
+        - id : id of the player
+    Return :
+        - A list of cases that can be claimed by the player, -1 otherwise
+    """
+    def get_zone(self, xy, id, zone_commune):
+        x_start, y_start = xy
+        zone = set()
+        for y in range(y_start - 1, y_start + 2):
+            for x in range(x_start - 1, x_start + 2):
+                if (x,y) not in zone_commune:
+                    id_case = self.get_case((x,y))
+                    if id_case != id and id_case != 0 and id_case != -1:
+                        return -1
+                    if id_case == 0:
+                        zone.add((x,y))
+                        zone_commune.add((x,y))
+
+        for case in zone:
+            zone_commune = self.get_zone(case, id, zone_commune)
+            if zone_commune == -1 :
+                return -1
+        return zone_commune
+
+    """
+    Method that fill all coord from zone
+    Args:
+        - zone : list of tuples containing coord of the board that can be claimed by a player
+        - player : The player who can claimed the case
+    """
+    def fill_zone(self, zone, player):
+        for case in zone:
+            self.update_case(case, player)
