6477a6284372ad3a4bee2017e6434c85cc8c9d7b
ThomasBtl <47348222+ThomasBtl@users.noreply.github.com>
2019-10-16 21:20:31 +0200

    Algo filling : bug correction

--- src/game_logic/game.py
+++ src/game_logic/game.py
@@ -32,27 +32,44 @@ class Game:
             return False
         if case_value == 0:
             self.update_case(xy, player)
+
+        self._zone_finding(xy, player)
+        return True
+
+    """
+    Method That find a zone to fill if there is one
+    Arg :
+        - xy : coord where the player want to go
+        - player : the player who is curently playing
+    """
+    def _zone_finding(self, xy, player):
         x,y = xy
         zone_right = set()
         zone_left = set()
-        # TODO : check diagonal
         delta = (x - player.x, y - player.y)
-        if(x == 0 or x == 7 or self.get_case((x + delta[0], y + delta[1])) == player.id):
-            zone_right = self.get_zone((x, y - 1), player.id, set((x,y)))
-            zone_left = self.get_zone((x, y + 1), player.id, set((x,y)))
-        elif(y == 0 or y == 7 or self.get_case((x + delta[0], y + delta[1])) == player.id):
-            zone_right = self.get_zone((x + 1, y), player.id, set((x,y)))
-            zone_left = self.get_zone((x - 1, y), player.id, set((x,y)))
+
+        if abs(delta[0]) == 1:
+            #Border_close is a boolean indicating that there is a border close to the player
+            border_close = any([True for row in range(-1,2) if self.get_case((x + delta[0], y + row)) == player.id or x == 0 or x == 7])
+            if(border_close):
+                zone_right = self._get_zone((x, y - 1), player.id, set((x,y)))
+                zone_left = self._get_zone((x, y + 1), player.id, set((x,y)))
+        else:
+            border_close = any([True for column in range(-1,2) if self.get_case((x + column, y + delta[1])) == player.id or y == 0 or y == 7])
+            if(border_close):
+                zone_right = self._get_zone((x + 1, y), player.id, set((x,y)))
+                zone_left = self._get_zone((x - 1, y), player.id, set((x,y)))
+
         if zone_left != -1:
-            self.fill_zone(zone_left, player)
+            self._fill_zone(zone_left, player)
         if zone_right != -1:
-            self.fill_zone(zone_right, player)
-        return True
+            self._fill_zone(zone_right, player)
+
 
     def start_game(self):
         self.players = Player(1, 0, 0), Player(2, 7, 7)
 
-        #I removed starting location validation, we can assume that we are not going to put the player on any comprise case
+        #Thomas : I removed starting location validation, we can assume that we are not going to put the player on any comprise case at the start
         self.update_case((0,0), self.players[0])
         self.update_case((self.size_x - 1, self.size_y - 1), self.players[1])
         self.players[0].xy = (0,0)
@@ -87,40 +104,39 @@ class Game:
     """
     Method that check if a zone is accessible by an other player
     Args :
-        - xy : coord xy of the player
-        - id : id of the player
+        - xy : coord where the player want to go
+        - id : id of the player curently playing
     Return :
         - A list of cases that can be claimed by the player, -1 otherwise
     """
-    def get_zone(self, xy, id, zone_commune):
+    def _get_zone(self, xy, id, commun_zone):
         x, y = xy
         zone = set()
         delta = -1
-        # TODO : Function opti here
         for _ in range(0,2):
             # Y check
-            if (x, y+delta) not in zone_commune:
+            if (x, y+delta) not in commun_zone:
                 id_case = self.get_case((x,y + delta))
                 if id_case != -1 and id_case != 0 and id_case != id:
                     return -1
                 if id_case == 0:
-                    zone_commune.add((x, y + delta))
+                    commun_zone.add((x, y + delta))
                     zone.add((x, y + delta))
             # X check
-            if (x + delta, y) not in zone_commune:
+            if (x + delta, y) not in commun_zone:
                 id_case = self.get_case((x + delta,y))
                 if id_case != -1 and id_case != 0 and id_case != id:
                     return -1
                 if id_case == 0:
-                    zone_commune.add((x + delta, y))
+                    commun_zone.add((x + delta, y))
                     zone.add((x + delta, y))
             delta = -delta
 
         for case in zone:
-            zone_commune = self.get_zone(case, id, zone_commune)
-            if zone_commune == -1 :
+            commun_zone = self._get_zone(case, id, commun_zone)
+            if commun_zone == -1 :
                 return -1
-        return zone_commune
+        return commun_zone
 
     """
     Method that fill all coord from zone
@@ -128,6 +144,6 @@ class Game:
         - zone : list of tuples containing coord of the board that can be claimed by a player
         - player : The player who can claimed the case
     """
-    def fill_zone(self, zone, player):
+    def _fill_zone(self, zone, player):
         for case in zone:
             self.update_case(case, player)
